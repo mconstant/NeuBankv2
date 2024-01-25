d63f6120d1ef74231a15e202a959fe7739186cd5
David West <1122691+dbwest@users.noreply.github.com>
2024-01-25 10:27:47 -0500

    bootstrap

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,6 +1,7 @@
 locals {
   resource_group_name    = "${var.github_user_name}-${var.github_repository}-${random_integer.sa_num.result}"
   storage_account_name   = "${lower(var.github_user_name)}${lower(var.github_repository)}${random_integer.sa_num.result}"
+  sas_account_name       = "${lower(var.github_user_name)}${lower(var.github_repository)}${random_integer.sa_num.result}sas"
   service_principal_name = "${var.github_user_name}-${var.github_repository}-${random_integer.sa_num.result}"
 }
 
@@ -46,6 +47,41 @@ resource "azurerm_storage_account" "sa" {
   location                      = var.azure_location
   account_tier                  = "Standard"
   account_replication_type      = "LRS"
+  # public_network_access_enabled = false
+}
+
+data "azurerm_storage_account_sas" "sas" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  https_only        = true
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = "2018-03-21T00:00:00Z"
+  expiry = "2025-03-21T00:00:00Z"
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = true
+    list    = true
+    add     = true
+    create  = true
+    update  = true
+    process = true
+    tag     = true
+    filter  = true
+  }
 }
 
 resource "azurerm_storage_container" "ct" {
@@ -64,6 +100,7 @@ resource "github_actions_secret" "actions_secret" {
     ARM_CLIENT_SECRET   = azuread_service_principal_password.gh_actions.value
     ARM_SUBSCRIPTION_ID = data.azurerm_subscription.current.subscription_id
     ARM_TENANT_ID       = data.azuread_client_config.current.tenant_id
+    ARM_SAS_TOKEN       = data.azurerm_storage_account_sas.sas.sas
   }
 
   repository      = var.github_repository
@@ -71,14 +108,15 @@ resource "github_actions_secret" "actions_secret" {
   plaintext_value = each.value
 }
 
-resource "null_resource" "local-provisioner" {
-  provisioner "local-exec" {
-    command = <<EOF
-      ACCOUNT_KEY=$(az storage account keys list --resource-group ${azurerm_storage_account.sa.resource_group_name} --account-name ${azurerm_storage_account.sa.name} --query '[0].value' -o tsv)
-      echo "export ARM_ACCESS_KEY=$ACCOUNT_KEY" >> ../.env
-      echo 'export STORAGE_ACCOUNT=${azurerm_storage_account.sa.name}' >> ../.env
-      echo 'export RESOURCE_GROUP=${azurerm_storage_account.sa.resource_group_name}' >> ../.env
-      echo 'export CONTAINER_NAME=${azurerm_storage_container.ct.name}' >> ../.env
-    EOF
-  }
-}
+# resource "null_resource" "local-provisioner" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#       ACCOUNT_KEY=$(az storage account keys list --resource-group ${azurerm_storage_account.sa.resource_group_name} --account-name ${azurerm_storage_account.sa.name} --query '[0].value' -o tsv)
+#       echo "export ARM_ACCESS_KEY=$ACCOUNT_KEY" >> ../.env
+#       echo 'export STORAGE_ACCOUNT=${azurerm_storage_account.sa.name}' >> ../.env
+#       echo 'export RESOURCE_GROUP=${azurerm_storage_account.sa.resource_group_name}' >> ../.env
+#       echo 'export CONTAINER_NAME=${azurerm_storage_container.ct.name}' >> ../.env
+#       echo 'export ARM_SAS_TOKEN="${data.azurerm_storage_account_sas.sas.sas}"' >> ../.env      
+#     EOF
+#   }
+# }
