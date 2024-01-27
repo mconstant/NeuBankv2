50feeb2cb4c12c19849c725f749b846ce51a67df
David West <1122691+dbwest@users.noreply.github.com>
2024-01-26 22:54:10 -0500

    change sa name

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_storage_account" "blob" {
   location                        = var.region
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
-  public_network_access_enabled   = false
+  # public_network_access_enabled   = false
   allow_nested_items_to_be_public = false
 
   sas_policy {
